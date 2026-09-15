a8d16600afef88984ae27441420b073c31d01b6b
BhavyaBhat2005 <bhavyabhat.work@gmail.com>
2026-09-15 10:39:39 +0530

    commit

--- 4.py
+++ 4.py
@@ -1,5 +1,7 @@
-n = input("Enter a number")
+n = int(input("Enter a number: "))
+
 number = 0
+
 while number <= 10:
-    print(n, "n",number," = ",n,*number)
-    number+=1
+    print(n, "x", number, "=", n * number)
+    number += 1
