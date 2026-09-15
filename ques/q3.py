7e1aadceb61cf1307208d405cda43639455b4eb1
BhavyaBhat2005 <bhavyabhat.work@gmail.com>
2026-09-15 11:30:10 +0530

    commit ques 3

--- ques/q3.py
+++ ques/q3.py
@@ -1,1 +1,13 @@
 #write a program to calculate the average of a given list
+
+numbers = [1,20,30,40]
+
+total = 0
+
+for num in numbers:
+    total += num
+
+average = total / len(numbers)
+
+print(numbers)
+print("Average =", average)
